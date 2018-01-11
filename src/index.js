5436a6b6f00e0c8e0f0fd2be155a54ae6af10ec4
Legendre <patatoche14@hotmail.com>
2018-01-11 16:57:32 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,3 +7,16 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+*/
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send({hi: 'there'});
+});
+
+//détermine le port, en dev le port peut être vide donc par défaut 5000
+const PORT= process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server started');
+});
