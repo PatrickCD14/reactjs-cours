ecd6e3ba1e4982dcccc42dd63104412b66b8b5fa
Legendre Patrick <patrick.legendre2@calvados.fr>
2018-01-25 13:45:23 +0100

    test

--- src/index.js
+++ src/index.js
@@ -9,14 +9,37 @@ ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 */
 const express = require("express");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send({hi: 'there'});
-});
+//console.developers.google.com
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 //détermine le port, en dev le port peut être vide donc par défaut 5000
-const PORT= process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('Server started');
 });
