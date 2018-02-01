75c64933add4f42295f3de827426cd0a931150e8
Legendre Patrick <patrick.legendre2@calvados.fr>
2018-02-01 15:29:31 +0100

    test

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,9 @@
-module.exports = {
-    googleClientID: '713474076729-63gq64vrvinvqivisn3atpgmj33c65q6.apps.googleusercontent.com',
-    googleClientSecret: 'ZsvEDfF5f7S_ZAQfUy-McTUb',
-    mongoURI: 'mongodb://reactjs:reactjs@ds115198.mlab.com:15198/emaily-cd14-dev'
-};
+if(process.env.NODE_ENV === 'production'){
+
+    module.exports = require('./prod');
+
+} else {
+
+    module.exports = require('./dev');
+
+}
