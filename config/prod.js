3c3dbd8d69b86332e3c8f4ead5ac66f249c23dff
Legendre Patrick <patrick.legendre2@calvados.fr>
2018-02-01 16:05:30 +0100

    test

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI
+    mongoURI: process.env.MONGO_URI,
+    googleRedirectURI: process.env.GOOGLE_REDIRECT_URI
 };
