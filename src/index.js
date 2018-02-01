60554c0b3fdb32f2467fd54d19b5975b75625ec3
Legendre Patrick <patrick.legendre2@calvados.fr>
2018-02-01 16:15:40 +0100

    test

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 require('../routes/authRoutes')(app);
 
-//détermine le port, en dev le port peut être vide donc par défaut 5000
+//détermine le port, en dev le port peut être vide donc par défaut 5000 test
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('Server started');
