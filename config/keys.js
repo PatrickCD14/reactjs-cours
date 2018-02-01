50418ea7d01e9b49295cdd3a33c992fd948dd0d0
Legendre Patrick <patrick.legendre2@calvados.fr>
2018-02-01 15:24:59 +0100

    ajout env var pour google et mongodb

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID: '713474076729-63gq64vrvinvqivisn3atpgmj33c65q6.apps.googleusercontent.com',
+    googleClientSecret: 'ZsvEDfF5f7S_ZAQfUy-McTUb',
+    mongoURI: 'mongodb://reactjs:reactjs@ds115198.mlab.com:15198/emaily-cd14-dev'
+};
