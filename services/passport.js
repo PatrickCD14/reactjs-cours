3be8f2a4fad55d7e9bd0de921203a7770afe1fe5
Legendre Patrick <patrick@thouleg.fr>
2018-02-03 18:48:28 +0100

    autentification complete

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
-console.log(keys);
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done)=> {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
+
 passport.use(
     new GoogleStrategy(
         {
@@ -11,9 +25,16 @@ passport.use(
             proxy:true
         },
         (accessToken , refreshToken, profile, done)=> {
-            console.log('access token', accessToken);
-            console.log('refresh token' , refreshToken);
-            console.log('profile', profile);
+            User.findOne({ googleId: profile.id }).then((existingUser) => {
+                    if(existingUser){
+                        done(null, existingUser);
+                    } else {
+                        console.log('new');
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, ));
+                    }
+                })
         }
 
     )
