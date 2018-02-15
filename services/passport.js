fa2dcd5d2dc0103ef084aacc12ff8dc6b5f5f837
patrick.legendre2@calvados.fr <48_Super_Bug>
2018-02-15 16:17:41 +0100

    essai sans proxy

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         (accessToken , refreshToken, profile, done)=> {
             User.findOne({ googleId: profile.id }).then((existingUser) => {
@@ -31,7 +32,7 @@ passport.use(
                         console.log('new');
                         new User({ googleId: profile.id })
                             .save()
-                            .then(user => done(null, ));
+                            .then(user => done(null, user));
                     }
                 })
         }
